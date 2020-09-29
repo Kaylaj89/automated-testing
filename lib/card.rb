ded82d83689e23d0839ab2430d3737c1c17ea4b5
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-28 18:26:48 -0700

    Made small change to expected input for def_to_s method

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,7 @@
 # card.rb
 SUITS = [:hearts, :spades, :clubs, :diamonds]
 VALUE_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-# FACE_CARD_VALUES = [1, 11, 12, 13]
-# FACE_CARDS = ["Ace", "Jack", "Queen", "King"]
+
 class Card
   attr_reader :value, :suit
 
@@ -15,25 +14,20 @@ class Card
 
   end
 
-  def self.true_value(value)
+  def self.uniform_values(value)
     new_value = nil
-
-    # if VALUE_CARDS.any? == value
-    #   return value
-    # end
-    new_value = value
+    new_value = value if VALUE_CARDS.any? value
     new_value = "Ace" if value == 1
     new_value = "Jack" if value == 11
     new_value = "Queen" if value == 12
     new_value = "King" if value == 13
-    new_value == value
     return new_value
 
   end
 
 
   def to_s
-      return "#{Card.true_value(value)} of #{suit.to_s}"
+      return "#{Card.uniform_values(value)} of #{suit.to_s}"
     end
 
 end
