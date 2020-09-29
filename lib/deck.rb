3e12a7cec2372b810062c52faaf84a9eb2d25bf3
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-28 22:25:05 -0700

    Added some code to draw method.

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,12 +8,19 @@ class Deck
   attr_reader :card
 
 
-  def initialize(value_cards, suits, face_cards)
-
+  def initialize
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+        return @cards
+      end
+    end
   end
 
   def draw
-    # returns a card
+    drawn_card = @cards.pop
+    return drawn_card
   end
 
   def shuffle
